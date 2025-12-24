53acbc2746f9396366aff7e97bf4bf966c7ee10b
hadeelomar <hadeelomar620@gmail.com>
2025-12-24 22:50:36 +0000

    Configuring AUTH_USER_MODEL in settings

--- project/settings.py
+++ project/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
     'default': database.config()
 }
 
+AUTH_USER_MODEL = 'api.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
