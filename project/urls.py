5d46b1a1fbfbeabffa504733b6571ca45c02e0a0
Hadeel Omar <hadeelomar620@gmail.com>
2026-01-18 21:42:43 +0000

    Rendering Vue for deployment

--- project/urls.py
+++ project/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
-from api.views import health
+from api.views import health, spa
 
 urlpatterns = [
     path('health', health),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path("", spa),
+    re_path(r"^(?!api/|admin/|health).*", spa)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
